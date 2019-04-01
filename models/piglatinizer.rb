873f96338b0b25fdb93778b435a97e964eb6b9d9
Grace Choi <oiograceoio@gmail.com>
2019-04-01 18:06:17 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,6 +4,8 @@ class PigLatinizer
     if words.split(" ").length == 1 #if one word
       if words[0].match(/[aAeEiIoOuU]/) #if starts with vowel
         piglatin = words+"way"
+      elsif !words[0].match(/[aAeEiIoOuU]/)
+        piglatin = word.slice(1..-1) + word.slice(0) + "ay"
       elsif !words[0].match(/[aAeEiIoOuU]/) && !words[1].match(/[aAeEiIoOuU]/) #if starts with 2 consonants
         piglatin = word.slice(2..-1) + word.slice(0,2) + "ay"
       end
