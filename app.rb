fd2b08e114a56e597613c45bd3de3f2dec36f6da
Grace Choi <oiograceoio@gmail.com>
2019-04-01 17:38:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    piglatin = PigLatinizer.new
+    word = PigLatinizer.new
+    @piglatin = word.piglatinize(params[:user_phrase])
 
   end
 
