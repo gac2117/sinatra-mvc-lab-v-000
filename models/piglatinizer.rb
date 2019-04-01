1e524222dd5e4bb4187fa6e2b93b3a86ebc354c9
Grace Choi <oiograceoio@gmail.com>
2019-04-01 17:33:59 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,11 @@
 class PigLatinizer
   attr_reader :words
 
-  def initialize(words=nil)
-    @words = words
+  def initialize(user_phrase)
+    @words = user_phrase
   end
 
-
+  def piglatinize 
+    
+  end
 end
