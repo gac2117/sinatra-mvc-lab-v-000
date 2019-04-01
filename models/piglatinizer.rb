43d2997e8fce97868d81ccf6a28d447efce6798b
Grace Choi <oiograceoio@gmail.com>
2019-04-01 18:06:37 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
       elsif !words[0].match(/[aAeEiIoOuU]/)
         piglatin = word.slice(1..-1) + word.slice(0) + "ay"
       elsif !words[0].match(/[aAeEiIoOuU]/) && !words[1].match(/[aAeEiIoOuU]/) #if starts with 2 consonants
-        piglatin = word.slice(2..-1) + word.slice(0,2) + "ay"
+        piglatin = words.slice(2..-1) + words.slice(0,2) + "ay"
       end
     end
   end
