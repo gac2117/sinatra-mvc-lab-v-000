f2b630440e7ab1a62cef67fce2193e10b76c0e7c
Grace Choi <oiograceoio@gmail.com>
2019-04-01 17:53:39 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,10 @@
 class PigLatinizer
 
   def piglatinize(words)
-
+    if words.split(" ").length == 1
+      first_letter = words[0]
+      words[0] = ""
+      piglatin = words+first_letter+"ay"
+      piglatin 
   end
 end
